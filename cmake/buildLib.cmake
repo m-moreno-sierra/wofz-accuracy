70c831f6bda5cf4ca97d330d183fee2bff945bd7
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2025-04-18 09:59:43 +0200

    indent

--- cmake/buildLib.cmake
+++ cmake/buildLib.cmake
@@ -25,8 +25,8 @@ if(NOT MSVC)
 endif()
 
 target_include_directories(${lib} INTERFACE
-  $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/lib>
-  $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>)
+    $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/lib>
+    $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>)
 
 install(
     TARGETS ${lib}
@@ -43,18 +43,18 @@ install(
 include(CMakePackageConfigHelpers)
 set(CMAKECONFIG_INSTALL_DIR "${CMAKE_INSTALL_LIBDIR}/cmake/cerf")
 configure_package_config_file(${PROJECT_SOURCE_DIR}/cmake/cerfConfig.cmake.in
-  "${CMAKE_CURRENT_BINARY_DIR}/cerfConfig.cmake"
-  INSTALL_DESTINATION ${CMAKECONFIG_INSTALL_DIR})
+    "${CMAKE_CURRENT_BINARY_DIR}/cerfConfig.cmake"
+    INSTALL_DESTINATION ${CMAKECONFIG_INSTALL_DIR})
 write_basic_package_version_file(${CMAKE_CURRENT_BINARY_DIR}/cerfConfigVersion.cmake
-  VERSION ${CERF_VERSION}
-  COMPATIBILITY SameMajorVersion)
+    VERSION ${CERF_VERSION}
+    COMPATIBILITY SameMajorVersion)
 install(FILES ${CMAKE_CURRENT_BINARY_DIR}/cerfConfig.cmake
-  ${CMAKE_CURRENT_BINARY_DIR}/cerfConfigVersion.cmake
-  DESTINATION ${CMAKECONFIG_INSTALL_DIR})
+    ${CMAKE_CURRENT_BINARY_DIR}/cerfConfigVersion.cmake
+    DESTINATION ${CMAKECONFIG_INSTALL_DIR})
 install(EXPORT ${intf}
-  NAMESPACE "cerf::"
-  FILE "cerfTargets.cmake"
-  DESTINATION ${CMAKECONFIG_INSTALL_DIR})
+    NAMESPACE "cerf::"
+    FILE "cerfTargets.cmake"
+    DESTINATION ${CMAKECONFIG_INSTALL_DIR})
 
 set(CPACK_PACKAGE_VENDOR "Forschungszentrum Jülich GmbH")
 set(CPACK_PACKAGE_VERSION ${CERF_VERSION})
