d1c20e479e1f49b89086185791ddc66c2fe4599a
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2025-10-28 09:53:05 +0100

    relax for other RS

--- test/dawsontest.c
+++ test/dawsontest.c
@@ -31,7 +31,7 @@
 #include "cerf.h"
 #include "testtool.h"
 
-const double abslim = 2.2e-14; // limit for relative error of absolute value
+const double abslim = 2.3e-14; // limit for relative error of absolute value
 const double cmplim = 8.8e-14; // limit for relative error of Re/Im components
 
 int main(void)
